e1ca8fc108708e04df9a93bc01d41b83385a3f44
MrStrugglez <42456773+MrStrugglez@users.noreply.github.com>
2025-02-24 13:52:18 +0200

    Initial Project Commit
    - Initial project containing basic login screen and feedback with no Firebase Auth

---
+++ android/app/src/main/kotlin/com/genetics/flutter_firebase_login/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.genetics.flutter_firebase_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/ui/screens/login_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: const Text('Login'))),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: _login, child: const Text('Login')),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _login() {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    if (email == "test@example.com" && password == "password123") {
+      print("Success");
+    } else {
+      print("failure");
+    }
+  }
+}
